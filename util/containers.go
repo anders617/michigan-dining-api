3fb3f4e5391d6f36cc7a61143fd66a83fdda8ac6
anders617 <anders617@gmail.com>
2019-10-21 01:28:07 -0400

    Updated //util/containers for work with []*struct to []interface{} conversion

--- util/containers.go
+++ util/containers.go
@@ -23,14 +23,22 @@ func Values(input interface{}) *[]interface{} {
 
 func AsSliceType(s interface{}, outSlice interface{}) interface{} {
 	// Some reflection magic that takes input array and converts to a different type
+	inT := reflect.TypeOf(s).Elem()
+	inElemT := inT.Elem()
 	inV := reflect.Indirect(reflect.ValueOf(s))
 
 	t := reflect.TypeOf(outSlice)
-	elemT := t.Elem()
+	outElemT := t.Elem()
 	out := reflect.MakeSlice(t, 0, inV.Len())
 
 	for idx := 0; idx < inV.Len(); idx++ {
-		out = reflect.Append(out, inV.Index(idx).Elem().Convert(elemT))
+		if outElemT.Kind() == reflect.Interface && inElemT.Kind() == reflect.Ptr {
+			// If out elem type is Interface and in elem type is ptr
+			// then no need to call .Elem() on inV since interfaces are ptrs to values
+			out = reflect.Append(out, inV.Index(idx))
+		} else {
+			out = reflect.Append(out, inV.Index(idx).Elem())
+		}
 	}
 	return out.Interface()
 }
