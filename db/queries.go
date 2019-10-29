6d4a61b4de69d55d503cfc08d8d9f707e44af7db
anders617 <anders617@gmail.com>
2019-10-29 19:03:41 -0400

    Refactor queries.go

--- db/queries.go
+++ db/queries.go
@@ -33,6 +33,7 @@ func (d *DynamoClient) QueryDiningHalls() (*pb.DiningHalls, error) {
 
 func (d *DynamoClient) QueryMenus(diningHallName *string, date *string, meal *string) (*[]*pb.Menu, error) {
 	glog.Infof("QueryMenus %v, %v, %v", diningHallName, date, meal)
+	// If we have all three then we can just do a Get since the key is fully specified
 	if diningHallName != nil && date != nil && meal != nil {
 		menu := pb.Menu{}
 		err := d.GetProto(MenuTableName, map[string]string{DateKey: *date, MenuTableDiningHallMealKey: *diningHallName + *meal}, &menu)
@@ -42,6 +43,7 @@ func (d *DynamoClient) QueryMenus(diningHallName *string, date *string, meal *st
 		menus := []*pb.Menu{&menu}
 		return &menus, nil
 	}
+	// If we are missing the meal, then do a PartitionKey lookup with BeginsWith condition on DiningHallMealKey for diningHallName
 	if date != nil && meal == nil {
 		keyCond := expression.Key(DateKey).Equal(expression.Value(*date))
 		if diningHallName != nil {
@@ -54,19 +56,9 @@ func (d *DynamoClient) QueryMenus(diningHallName *string, date *string, meal *st
 			ExpressionAttributeValues: expr.Values(),
 			TableName:                 aws.String(MenuTableName),
 		}
-		req := d.client.QueryRequest(params)
-		result, err := req.Send(context.Background())
-		if err != nil {
-			return nil, err
-		}
-		menus := make([]*pb.Menu, len(result.Items))
-		for idx, item := range result.Items {
-			menu := pb.Menu{}
-			dynamodbattribute.UnmarshalMap(item, &menu)
-			menus[idx] = &menu
-		}
-		return &menus, nil
+		return d.queryMenus(params)
 	}
+	// If we are missing diningHallName do a PartitionKey lookup with filter expression for meal
 	if date != nil && meal != nil && diningHallName == nil {
 		keyCond := expression.Key(DateKey).Equal(expression.Value(*date))
 		filter := expression.Name("meal").Equal(expression.Value(*meal))
@@ -78,18 +70,24 @@ func (d *DynamoClient) QueryMenus(diningHallName *string, date *string, meal *st
 			ExpressionAttributeValues: expr.Values(),
 			TableName:                 aws.String(MenuTableName),
 		}
-		req := d.client.QueryRequest(params)
-		result, err := req.Send(context.Background())
-		if err != nil {
-			return nil, err
-		}
-		menus := make([]*pb.Menu, len(result.Items))
-		for idx, item := range result.Items {
-			menu := pb.Menu{}
-			dynamodbattribute.UnmarshalMap(item, &menu)
-			menus[idx] = &menu
-		}
-		return &menus, nil
+		return d.queryMenus(params)
 	}
+	// If we are missing date, we have to do a scan with filter
 	return nil, errors.New("Unimplemented Menu Query")
+}
+
+// Execute a query with the given parameters and marshal the output into a slice of *pb.Menu
+func (d *DynamoClient) queryMenus(params *dynamodb.QueryInput) (*[]*pb.Menu, error) {
+	req := d.client.QueryRequest(params)
+	result, err := req.Send(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	menus := make([]*pb.Menu, len(result.Items))
+	for idx, item := range result.Items {
+		menu := pb.Menu{}
+		dynamodbattribute.UnmarshalMap(item, &menu)
+		menus[idx] = &menu
+	}
+	return &menus, nil
 }
